5c79ddcc8ffcd4ce2dcff633f0f3c29670b47fce
mohammad <mohammad.vazirpanah@yahoo.com>
2021-08-22 17:08:08 +0430

    Simple Examples

---
+++ Simple Examples/Test1.py
@@ -0,0 +1,7 @@
+number = int(input("Please Give Me Your Number? "))
+
+print("Binary Value Is %s" % (bin(number)))
+
+print("Octal Value Is %s" % (oct(number)))
+
+print("Hexadecimal Value Is %s" % (hex(number)))
